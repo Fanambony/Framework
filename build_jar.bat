6555fad70d1fa78a45ec6162bb245ffc05b2fa84
Fanambony <ramarosonbenjamina@gmail.com>
2023-03-28 19:55:09 +0300

    Sprint4

--- build_jar.bat
+++ build_jar.bat
@@ -1,5 +1,5 @@
 jar -cf fw.jar -C .\Framework\build\web\WEB-INF\classes\ etu2023
-jar -cvf test_fw.war -C .\Test-Framework\build\web\ WEB-INF\
+jar -cvf test_fw.war -C .\Test_Framework\build\web\ WEB-INF\
 copy test_fw.war C:\MyWebServer\apache-tomcat-10.0.20\apache-tomcat-10.0.20\webapps\
-xcopy fw.jar D:\Etude\L2\S4\INF209-Web_dynamique\project\Step_to_Framework\Test-Framework\build\web\WEB-INF\lib\
-xcopy D:\Etude\L2\S4\INF209-Web_dynamique\project\Step_to_Framework\Test-Framework\build\web\ D:\Logiciel\Apache_Software_Foundation\Tomcat_10.0\webapps\Framework\ /E
+xcopy fw.jar D:\Documents\MrNaina\S4\Framework(1)\Test_framework\build\web\WEB-INF\lib\
+xcopy D:\Documents\MrNaina\S4\Framework(1)\Test_framework\build\web\ C:\MyWebServer\apache-tomcat-10.0.20\apache-tomcat-10.0.20\webapps\Framework\ /E
