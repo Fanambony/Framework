7df10f172eb272be38c74ebc56175ce2cbda3feb
Fanambony <ramarosonbenjamina@gmail.com>
2023-05-09 10:52:24 +0300

    script

--- build_jar.bat
+++
@@ -1,5 +0,0 @@
-jar -cf fw.jar -C .\Framework\build\web\WEB-INF\classes\ etu2023
-jar -cvf test_fw.war -C .\Test_Framework\build\web\ WEB-INF\
-copy test_fw.war C:\MyWebServer\apache-tomcat-10.0.20\apache-tomcat-10.0.20\webapps\
-xcopy fw.jar D:\Documents\MrNaina\S4\Framework(1)\Test_framework\build\web\WEB-INF\lib\
-xcopy D:\Documents\MrNaina\S4\Framework(1)\Test_framework\build\web\ C:\MyWebServer\apache-tomcat-10.0.20\apache-tomcat-10.0.20\webapps\Framework\ /E
